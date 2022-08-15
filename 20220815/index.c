12ae88634620332d39153f7f4dcf16f1501a5efd
huang <634475859@qq.com>
2022-08-16 01:17:27 +0800

    编程的初始,hello world, 预处理指令

---
+++ 20220815/index.c
@@ -0,0 +1,13 @@
+#include <stdio.h> // #表示预处理指令  这句告诉程序在编译之前要先执行哪些操作 大多数放在程序源文件之前
+
+// int main(void) 
+// {
+//   printf("\" If at first you don't succeed, try, try, try again !");
+//   return 0;
+// }
+
+int main(void)
+{
+  printf("\" If at first you don't succeed, try, try, try again !");
+  return 0;
+}
